21de85178e20faf6529b463167099e4490065eca
Saori Mita <saorimita4@gmail.com>
2022-09-05 19:45:06 +0200

    destroy review

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'restaurants#index'
   resources :restaurants do
+    resources :reviews, only: %i[new create]
     collection do
       get :top
     end
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
       get :chef
     end
   end
+  resources :reviews, only: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_164227) do
+ActiveRecord::Schema.define(version: 2022_09_05_170350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,13 @@ ActiveRecord::Schema.define(version: 2022_09_05_164227) do
     t.string "chef"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
